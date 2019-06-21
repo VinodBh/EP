abca7f537a639f86be686809b792478aaa7e1d54
Vinod Machhindra Bhavsar <VB00545738@techmahindra.com>
2019-06-21 12:09:38 +0530

    webdriver changes added

--- src/tests/TestPolicy.java
+++ src/tests/TestPolicy.java
@@ -1,16 +1,26 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestPolicy {
 
 	
-	
+	public WebDriver d;
 	@BeforeMethod
 	public void beforeMethod() {
 		System.setProperty("webdriver.chrome.driver", "D:\\Eclipse-Workspace\\TestEP\\Resources\\chromedriver.exe");
 		
+	}
+	
+	@BeforeMethod
+	public void afterMethod() {
+		if(d != null) {
+			d.quit();
+			
+		}
 		
 	}
 	
@@ -19,6 +29,9 @@ public class TestPolicy {
 	@Test
 	public void policyTestOne() {
 		System.out.println("i am in policyTestOne");
+		d = new ChromeDriver();
+		d.get("https://www.google.com");
+		
 	}
 	
 	
