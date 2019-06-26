b195e2cb0c8807662c5c6ea988ed896c3db6d5df
VinodBh <41318368+VinodBh@users.noreply.github.com>
2019-06-26 12:00:58 +0530

    Update TestSort1.java

--- src/tests/TestSort1.java
+++ src/tests/TestSort1.java
@@ -15,7 +15,7 @@ al.add(new Student(104,"Vinod",24));
 al.add(new Student(103,"Vinay",24));  
 al.add(new Student(106,"Aanand",24));  
 al.add(new Student(101,"jay",23)); 
-al.add(new Student(101,"New line added, modified later",260619)); 
+al.add(new Student(101,"New line added, modified later, modified2",260619)); 
 Collections.sort(al);
 
 for(Student st:al){  
