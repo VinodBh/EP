62d5e8aac95e99d0acc6de38c7e6c5dfd8af76dc
Vinod <vinodbhavsar23@gmail.com>
2019-08-04 16:28:08 +0530

    second commit

--- src/tests/TestSort1.java
+++ src/tests/TestSort1.java
@@ -6,10 +6,8 @@ public class TestSort1 {
 	
 public static void main(String args[]){  
 
+ArrayList<Student> al=new ArrayList<Student>();
 
-
-ArrayList<Student> al=new ArrayList<Student>();  
- 
 al.add(new Student(102,"Ajay",22));  
 al.add(new Student(104,"Vinod",24));  
 al.add(new Student(103,"Vinay",24));  
@@ -17,21 +15,23 @@ al.add(new Student(106,"Aanand",24));
 al.add(new Student(101,"jay",23)); 
 al.add(new Student(101,"New line added, modified later, modified2",260619));
 al.add(new Student(105,"New line added on 4 Aug 19 ", 40819));
+<<<<<<< HEAD
 Collections.sort(al);// do not delete
 
 
 
 
+=======
+al.add(new Student(105,"New line2 added on 4 Aug 19 ", 40819-2));
+Collections.sort(al); // COMMENTADDED.
+>>>>>>> second commit
 for(Student st:al){  
 System.out.println(st.rollno+" "+st.name+" "+st.age);
 }
-
-
 System.out.println("SuM-----" + Student.sum(al, myEnum.ROLLNO.toString()));
 	char[] chars = new char[] {'\u0098'};
     String str = new String(chars);
     byte[] bytes = str.getBytes();
     System.out.println("new code"+ Arrays.toString(bytes));
-    
 }  
 }  
